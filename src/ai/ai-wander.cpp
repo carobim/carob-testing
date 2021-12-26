c3319e85f60361d0f706f99b0ebbffddea0bfc47
Paul Merrill <pdm@pdm.me>
2021-12-26 14:52:19 -0800

    Use tiles/ header paths and remove file comments

--- src/ai/ai-wander.cpp
+++ src/ai/ai-wander.cpp
@@ -1,35 +1,8 @@
-/**************************************
-** Tsunagari Tile Engine             **
-** ai-wander.cpp                     **
-** Copyright 2014      Michael Riley **
-** Copyright 2014-2020 Paul Merrill  **
-**************************************/
-
-// **********
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
-// **********
-
 #include "ai/ai-wander.h"
 
-#include "core/character.h"
-#include "core/client-conf.h"
-#include "core/cooldown.h"
+#include "tiles/character.h"
+#include "tiles/client-conf.h"
+#include "tiles/cooldown.h"
 #include "util/assert.h"
 #include "util/random.h"
 
